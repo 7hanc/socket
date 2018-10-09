d64edb814358810524bedf1baea20db6ee67726f
7hanc <43276098+7hanc@users.noreply.github.com>
2018-10-09 20:37:18 +0800

    Create client.py

---
+++ client.py
@@ -0,0 +1,24 @@
+
+import socket
+
+def client():
+    host = socket.gethostname()  
+    port = 5000 
+    
+    client_socket = socket.socket()  
+    client_socket.connect((host, port))  
+    
+    message = input(" -> ") 
+    while message!='bye':
+         
+        client_socket.send(message.encode())  
+        server_time = client_socket.recv(1024) 
+        str_length =  client_socket.recv(1024)
+     
+        print ('Time of server:', server_time)
+        print('Length of string:', str_length)
+        message = input(" -> ") 
+        
+    client_socket.close()  
+    
+client()
